25bbca192aaaad330779d658cea8bc1685993dbe
Vili <59320967+V1li@users.noreply.github.com>
2023-05-16 16:49:11 +0000

    Update ezd.bat

--- ezd.bat
+++ ezd.bat
@@ -8,26 +8,30 @@ echo
 echo
 echo "Welcome to Ez'd utility tool"
 echo "Easy way to gather useful information about your current Windows device."
-echo
+echo "This will also print the data to txt files for later inspection."
 echo
 echo "Starting in 3 seconds..."
 timeout /t 3 /nobreak
 echo "Checking for users"
+net users
 net users > users.txt
 echo
 echo "Done, next check in 3 secs..."
 timeout /t 3 /nobreak
 echo "Checking for local groups"
+net localgroup
 net localgroup > groups.txt
 echo
 echo "Done, next check in 3 secs..."
 timeout /t 3 /nobreak
 echo "Checking for applied policies"
+gpresult /Scope User /v
 gpresult /Scope User /v > policies.txt
 echo
 echo "Done, next check in 3 secs..."
 timeout /t 3 /nobreak
 echo "Checking for saved connections"
+netsh wlan show all
 netsh wlan show all > networks.txt
 
 set /p input= "Press ENTER to exit."
